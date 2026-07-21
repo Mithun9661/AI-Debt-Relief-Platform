c4d269199c8267494c02768716ec6c9623904db7
Mithun Kumar <mithunk98991@gmail.com>
2026-07-21 05:44:03 +0530

    Fix jose import syntax

--- backend/app/auth_utils.py
+++ backend/app/auth_utils.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from jose import jwtfrom jose import jwt
+from jose import jwt
 from passlib.context import CryptContext
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret")
